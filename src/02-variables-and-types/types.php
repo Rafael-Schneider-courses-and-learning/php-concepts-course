55563dd4e672f62016780274a23946c6934f5d9b
Rafael Vinícius Schneider <rafaelvschneider@gmail.com>
2022-08-19 23:43:06 -0300

    primitive types lesson

---
+++ src/02-variables-and-types/types.php
@@ -0,0 +1,12 @@
+<?php
+
+$age = 21; // integer
+$salary = 1000.305; // double
+$result = 10 / 2; // double
+$result = 3; // integer
+
+$text = "Hello World"; // string
+
+$isRaining = false; // boolean
+
+echo gettype($text);
