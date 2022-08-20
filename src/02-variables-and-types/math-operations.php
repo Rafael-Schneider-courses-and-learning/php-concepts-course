040a11489847bd8b12df5647f79234d253725e9a
Rafael Vinícius Schneider <rafaelvschneider@gmail.com>
2022-08-19 23:19:49 -0300

    php math operations lesson

---
+++ src/02-variables-and-types/math-operations.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Addition: 2 + 2
+ * Subtraction: 2 - 2
+ * Multiplication: 2 * 2
+ * Division: 2 / 2
+ * Exponentiation: 2 ** 3, equivalent to 2 * 2 * 2
+ * Modulo (remainder of division): 10 % 3
+ */
+
+$age = 21;
+$ageTenYearsFromNow = $age + 10;
+$ageFiveYearsAgo = $age - 5;
+
+$yearBecameDeveloper = 2019;
+$experienceYears = 2022 - $yearBecameDeveloper;
+
+echo $experienceYears;
