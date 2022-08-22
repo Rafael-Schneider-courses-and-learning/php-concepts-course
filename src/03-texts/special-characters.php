e48907be817c725d90a2b3c0f79b8001941b4086
Rafael Vinícius Schneider <rafaelvschneider@gmail.com>
2022-08-21 21:55:44 -0300

    special chars lesson

---
+++ src/03-texts/special-characters.php
@@ -0,0 +1,10 @@
+<?php
+
+$age = 21;
+$name = 'Rafael';
+
+echo "Olá mundo!" . PHP_EOL;
+echo "Me chamo $name e tenho $age anos." . PHP_EOL;
+
+// echo "\t isso é um tab";
+// echo "Isso é uma quebra de linha \n";
