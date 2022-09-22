c54b6c3b6733fb0745d4d00bcbce421311471ccb
Rafael Vinícius Schneider <rafaelvschneider@gmail.com>
2022-09-21 23:32:01 -0300

    else structure lesson

--- src/04-making-decisions/decisions.php
+++ src/04-making-decisions/decisions.php
@@ -6,5 +6,8 @@ echo "Você só pode entrar se tiver a partir de 18 anos." . PHP_EOL;
 
 if ($age >= 18) {
     echo "Você tem $age anos." . PHP_EOL;
-    echo "Pode entrar!";
+    echo "Pode entrar!" . PHP_EOL;
+} else {
+    echo "Você só tem $age anos." . PHP_EOL;
+    echo "Você não pode entrar!" . PHP_EOL;
 }
